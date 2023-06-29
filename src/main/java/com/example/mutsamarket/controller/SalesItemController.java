3c649898f7db82eb12a203cdce0c0d223cd013f5
Jyoungjo <zimbote0411@gmail.com>
2023-06-30 00:59:34 +0900

    feat: SalesItem - Controller 기능 구현 - CRUD 관련 Mapping - 파라미터 설정 (Pageable, MultipartFile)

--- src/main/java/com/example/mutsamarket/controller/SalesItemController.java
+++ src/main/java/com/example/mutsamarket/controller/SalesItemController.java
@@ -1,11 +1,59 @@
 package com.example.mutsamarket.controller;
 
+import com.example.mutsamarket.dto.ResponseDto;
+import com.example.mutsamarket.dto.SalesItemDto;
+import com.example.mutsamarket.service.SalesItemService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/items")
 public class SalesItemController {
+
+    private final SalesItemService itemService;
+
+    @PostMapping
+    public ResponseDto create(@RequestBody SalesItemDto dto) {
+        return itemService.addItem(dto);
+    }
+
+    @GetMapping
+    public Page<SalesItemDto> readAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "limit", defaultValue = "20") Integer limit
+    ) {
+        return itemService.readAllItems(page, limit);
+    }
+
+    @GetMapping("/{itemId}")
+    public SalesItemDto readOne(
+            @PathVariable("itemId") Long itemId
+    ) {
+        return itemService.readOneItem(itemId);
+    }
+
+    @PutMapping("/{itemId}")
+    public ResponseDto update(
+            @PathVariable("itemId") Long itemId, @RequestBody SalesItemDto dto
+    ) {
+        return itemService.updateItemInfo(itemId, dto);
+    }
+
+    @PutMapping(value = "/{itemId}/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseDto updateImage(
+            @PathVariable("itemId") Long itemId, @RequestParam("image") MultipartFile itemImage
+    ) {
+        return itemService.updateItemImage(itemId, itemImage);
+    }
+
+    @DeleteMapping("/{itemId}")
+    public ResponseDto delete(
+            @PathVariable("itemId") Long itemId, @RequestBody SalesItemDto dto
+    ) {
+        return itemService.deleteItem(itemId, dto);
+    }
 }
