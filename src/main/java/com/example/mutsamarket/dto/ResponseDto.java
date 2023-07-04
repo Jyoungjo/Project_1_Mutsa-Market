1ce2df6f79db345820a7c6a12286d61251ad70ee
Jyoungjo <zimbote0411@gmail.com>
2023-07-04 22:37:48 +0900

    refactoring: ResponseDto 객체 생성 메소드 추가

--- src/main/java/com/example/mutsamarket/dto/ResponseDto.java
+++ src/main/java/com/example/mutsamarket/dto/ResponseDto.java
@@ -9,4 +9,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ResponseDto {
     private String message;
+
+    public static ResponseDto getInstance(String message) {
+        ResponseDto responseDto = new ResponseDto();
+        responseDto.setMessage(message);
+        return responseDto;
+    }
 }
