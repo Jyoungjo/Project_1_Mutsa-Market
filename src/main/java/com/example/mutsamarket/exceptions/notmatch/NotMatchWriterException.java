d50c7453cc89c58a2f16479a73070f1759157639
Jyoungjo <zimbote0411@gmail.com>
2023-07-28 02:32:03 +0900

    remove: 사용하지 않는 파일 삭제

--- src/main/java/com/example/mutsamarket/exceptions/notmatch/NotMatchWriterException.java
+++
@@ -1,9 +0,0 @@
-package com.example.mutsamarket.exceptions.notmatch;
-
-import com.example.mutsamarket.exceptions.Status400Exception;
-
-public class NotMatchWriterException extends Status400Exception {
-    public NotMatchWriterException() {
-        super("작성자가 일치하지 않습니다.");
-    }
-}
