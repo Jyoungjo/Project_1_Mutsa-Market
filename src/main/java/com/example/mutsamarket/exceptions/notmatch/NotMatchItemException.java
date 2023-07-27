727d575f47e48ff3eb2ff06de2d5e5f7136ae51d
Jyoungjo <zimbote0411@gmail.com>
2023-07-27 14:52:03 +0900

    rename: 연관 관계 매핑에 따른 파일 이름 수정 - NotMatchItemIdException -> NotMatchItemException

--- src/main/java/com/example/mutsamarket/exceptions/notmatch/NotMatchItemException.java
+++ src/main/java/com/example/mutsamarket/exceptions/notmatch/NotMatchItemException.java
@@ -2,8 +2,8 @@ package com.example.mutsamarket.exceptions.notmatch;
 
 import com.example.mutsamarket.exceptions.Status400Exception;
 
-public class NotMatchItemIdException extends Status400Exception {
-    public NotMatchItemIdException() {
+public class NotMatchItemException extends Status400Exception {
+    public NotMatchItemException() {
         super("해당 물품이 아닙니다.");
     }
 }
